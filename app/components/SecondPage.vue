4db71bce24abfb969b53b9f7a4b3e66bd2035cc5
ll9 <llwierer@web.de>
2018-12-12 21:37:25 +0100

    good performance with listview

--- app/components/SecondPage.vue
+++ app/components/SecondPage.vue
@@ -1,70 +1,50 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <ScrollView>
-            <StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-                <StackLayout>
-                    <FloatText v-for="n in 5" :key="n" :hint="n" />
-                </StackLayout>
-            </StackLayout>
-        </ScrollView>
-    </Page>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <ScrollView>
+        <ListView for="item in arr">
+          <v-template>
+            <FloatText :keyboardType="item % 2 == 0 ? 'number': 'url'" :hint="item"/>
+          </v-template>
+        </ListView>
+    </ScrollView>
+  </Page>
 </template>
 
 <script>
-import FloatText from './sub/FloatText';
+import FloatText from "./sub/FloatText";
 
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    },
-    components: {
-        FloatText
-    }
+export default {
+  data() {
+    return {
+      arr: Array.from(Array(40).keys()),
+      msg: "Hello World!"
+    };
+  },
+  components: {
+    FloatText
   }
+};
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
 
-    StackLayout {
-        padding: 10;
-        margin: 10;
-        border-color: black;
-        border-width: 3;
-        border-radius: 5;
-    }
+StackLayout {
+  padding: 10;
+  margin: 10;
+  border-color: black;
+  border-width: 3;
+  border-radius: 5;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
 </style>
